c90f8f1b24218a50049673514427b32187fb51db
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-23 01:44:43 +0530

    Add project files.

---
+++ UserRegistrationMSTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserRegistrationMSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration Test With MS Test");
+        }
+    }
+}
